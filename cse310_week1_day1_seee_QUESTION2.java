6b995b6ce8a6adebed9fb62889c0c2dbd9c6a2ec
SuryabhanSinghAndotra <125852883+SuryabhanSinghAndotra@users.noreply.github.com>
2023-04-29 01:40:25 +0530

    Create cse310_week1_day1_seee_QUESTION2.java

---
+++ cse310_week1_day1_seee_QUESTION2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        double n=sc.nextDouble();
+        double m=sc.nextDouble();
+        double l=sc.nextDouble();
+        double a=((n+m+l)/3);
+        System.out.printf("%.1f",a-0.1);
+    }
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Term
